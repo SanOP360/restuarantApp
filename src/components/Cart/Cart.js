118b5913984dcaf8ed500747c59b04886b4539f1
SanOP360 <sanjayl2001alm@gmail.com>
2024-01-05 21:21:16 +0530

    added modal

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
+
+const Cart = (props) => {
+  const cartItems = [{ id: "c1", name: "Chicken", amount: 2, price: 1222 }].map(
+    (item) => <li key={item.id}>{item.name}</li>
+  );
+
+  return (
+    <Modal>
+      <ul className={classes["cart-item"]}>{cartItems}</ul>
+
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>344</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button-alt"]}>Close</button>
+        <button className={classes.orderBtn}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
--- src/components/Meals/MealForm.js
+++ src/components/Meals/MealForm.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import classes from './MealForm.module.css'
+import React, { useState } from 'react';
+import classes from './MealForm.module.css';
 
 import Input from '../UI/Input';
-const MealForm=()=>{
-    return (
-      
-        <form className={classes.form}>
-            <Input label="Amount" input={{
-                id:'amount',
-                type:'number',
-                min:'1',
-                max:'5',
-                step:'1',
-                defaultValue:'1'
-            }} />
-          
 
-          <button className={classes.addBtn}>+ Add</button>
-        </form>
-      
-    );
-}
+const MealForm = () => {
+  const [initialVal, setInitialVal] = useState('1');
 
-export default MealForm
+  const addMealHandler = () => {
+    setInitialVal()
+  };
+
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: 'amount',
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: initialVal,
+        }}
+      />
+
+      <button className={classes.addBtn} type="button" onClick={addMealHandler}>
+        + Add
+      </button>
+    </form>
+  );
+};
+
+export default MealForm;
